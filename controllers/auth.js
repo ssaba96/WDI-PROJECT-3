aed287be24117e4ca19dabaf508289e5ad8bf1b0
Chavez King <chavezking93@gmail.com>
2018-06-22 15:27:01 +0100

    login-register functions in controllers/auth.js

---
+++ controllers/auth.js
@@ -0,0 +1,24 @@
+const User = require('../models/user');
+
+
+
+function register(req, res, next) {
+  User.create(req.body)
+    .then(user => res.json(user))
+    .catch(next);
+
+}
+
+function login(req, res, next) {
+  User.findOne({ email: req.body.email })
+    .then(user => {
+      if(!user || !user.validatePassword(req.body.password)) {
+        return res.status(401).json({ message: 'Unauthorized'});
+      }
+    });
+}
+
+module.exports = {
+  register,
+  login
+};
