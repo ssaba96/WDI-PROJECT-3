a0d6b77a5ff79a8e50806b45027587e7054f6025
Family <sabziamjad@gmail.com>
2018-06-23 19:13:17 +0100

    created routes for home, index, show, login, register

---
+++ src/config/routes.js
@@ -0,0 +1,33 @@
+function Router($stateProvider, $urlRouterProvider) {
+  $stateProvider
+    .state('home', {
+      url: '/',
+      templateUrl: './views/home.html',
+      controller: 'HomeCtrl'
+    })
+    .state('museumsIndex', {
+      url: '/museums',
+      templateUrl: './views/museums/index.html',
+      controller: 'MuseumsIndexCtrl',
+      params: { search: null }
+    })
+    .state('museumsShow', {
+      url: '/museums/:id',
+      templateUrl: './views/museums/show.html',
+      controller: 'MuseumsShowCtrl'
+    })
+    .state('login', {
+      url: '/login',
+      templateUrl: './views/auth/login.html',
+      controller: 'AuthLoginCtrl'
+    })
+    .state('register', {
+      url: '/register',
+      templateUrl: './views/auth/register.html',
+      controller: 'AuthRegisterCtrl'
+    });
+
+  $urlRouterProvider.otherwise('/');
+}
+
+export default Router;
