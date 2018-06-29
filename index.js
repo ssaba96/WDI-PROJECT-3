0a53e4db4adb6bb87cb25a64f1e2f06d0b7e77f9
Chavez King <chavezking93@gmail.com>
2018-06-29 11:24:11 +0100

    Moved catchall handler

--- index.js
+++ index.js
@@ -15,8 +15,8 @@ app.use(express.static(`${__dirname}/public`));
 
 
 app.use(bodyParser.json());
-app.get('/*',(req, res) => res.sendFile(`${__dirname}/public/index.html`)); 
 app.use('/api', routes);
+app.get('/*',(req, res) => res.sendFile(`${__dirname}/public/index.html`)); 
 
 
 
