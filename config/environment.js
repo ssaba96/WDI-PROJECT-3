434fe0d83d09e74bd7d8baec34c41f247cf1d0df
Chavez King <chavezking93@gmail.com>
2018-06-29 11:13:40 +0100

    updating environment

--- config/environment.js
+++ config/environment.js
@@ -1,6 +1,6 @@
-const port = 4000;
+const port = process.env.PORT || 4000;
 const env = process.env.NODE_ENV || 'dev';
-const dbURI = `mongodb://localhost/museum-api-${env}`;
+const dbURI = process.env.MONGODB_URI || `mongodb://localhost/museum-api-${env}`;
 const secret = 'dsde=-03ddcd';
 
 module.exports = { port, dbURI, secret };
